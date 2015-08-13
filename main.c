0fb6647236e127868f93cd38144a5b67c26c3794
geppettodivacin <ericdilmore@gmail.com>
2015-08-13 14:36:20 -0500

    Make test for registering and freeing components.

--- main.c
+++ main.c
@@ -23,16 +23,50 @@
 #include "component.h"
 #include "world.h"
 
+
+struct One
+{
+    int x;
+};
+typedef struct One One;
+
+void oneInit ( void * component )
+{
+    One * one = ( One * ) component;
+    one->x = 10;
+}
+
 int main ( int argc, char *argv[] )
 {
     World * world = newWorld();
+    registerComponent ( oneInit, COMPONENT_ONE, sizeof ( One ), world );
+
     Entity e = newEntity ( world );
+
+    printf ( "Entity %d %s COMPONENT_ONE\n", e
+           , hasComponent ( e, COMPONENT_ONE, world )
+           ? "has" : "does not have" );
     addComponent ( e, COMPONENT_ONE, world );
-    printMask ( world->mask[ e ] );
-    printf ( "Next entity: %d\n", newEntity ( world ) );
-    removeComponent ( e, COMPONENT_ONE, world );
-    printMask ( world->mask[ e ] );
-    printf ( "Next entity: %d\n", newEntity ( world ) );
+    printf ( "Entity %d %s COMPONENT_ONE\n", e
+           , hasComponent ( e, COMPONENT_ONE, world )
+           ? "has" : "does not have" );
+    printf ( "Entity %d %s COMPONENT_ONE\n", 1
+           , hasComponent ( 1, COMPONENT_ONE, world )
+           ? "has" : "does not have" );
+
+    One * one = ( One * ) getComponent ( e, COMPONENT_ONE, world );
+    if ( !one )
+    {
+        printf ( "Entity does not have the requested component.\n" );
+        freeWorld ( world );
+        return EXIT_FAILURE;
+    }
+
+    one->x = 5;
+
+    One * two = ( One * ) getComponent ( e, COMPONENT_ONE, world );
+    printf ( "This entity has COMPONENT_ONE with an x of %d.\n", two->x );
+
     freeWorld ( world );
     return EXIT_SUCCESS;
 }
