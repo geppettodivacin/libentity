5663a6c8b7d65ccfb9692772f606ef7ed7a0d1ed
geppettodivacin <ericdilmore@gmail.com>
2015-08-20 08:31:56 -0500

    Comment world.h and world.c.

--- world.c
+++ world.c
@@ -25,59 +25,74 @@
 #include "mask.h"
 #include "component.h"
 
-unsigned int ENTITY_COUNT = 128;
+unsigned int ENTITY_COUNT = 128; // the default maximum number of entities
 
+// Returns a new, empty world.
 World * newWorld ( void )
 {
     int i;
+    // Allocate memory for the world.
     World * world = ( World * ) malloc ( sizeof ( *world ) );
 
+    // Allocate memory for masks.
     world->mask = ( Mask * ) malloc ( ENTITY_COUNT * sizeof ( *world->mask ) );
 
+    // Ensure that all masks are cleared to begin with.
     for ( i = 0; i < ENTITY_COUNT; ++i )
     {
         maskCopy ( world->mask[ i ], COMPONENT_NONE );
     }
 
+    // Ensure that all components are clear.
     for ( i = 0; i < 32 * MASK_LENGTH; ++i )
     {
         world->component[ i ] = NULL;
         world->componentSize[ i ] = 0;
     }
 
+    // The first entity that should be allocated is 0.
     world->nextEntity = 0;
 
     return world;
 }
 
+// Free the world and all data.
 void freeWorld ( World * world )
 {
+    // Free the mask array.
     free ( world->mask );
 
+    // Ensure that all component arrays are freed.
     int i = 0;
     for ( i = 0; i < 32 * MASK_LENGTH; ++i )
     {
         if ( world->component[ i ] ) free ( world->component[ i ] );
     }
 
+    // Free the world itself.
     free ( world );
 }
 
+// Shift entities down to the front of the array.
 void shrinkWorld ( World * world )
 {
     Entity e = 0;
     Entity f = ENTITY_COUNT - 1;
+    // Search for the first blank component.
     for ( e = 0; e < f; ++e )
     {
         if ( maskEqual ( world->mask[ e ], COMPONENT_NONE ) )
         {
+            // Search for the last non-blank component.
             for ( ; f > e; --f )
             {
                 if ( !maskEqual ( world->mask[ f ], COMPONENT_NONE ) )
                 {
+                    // Swap the masks.
                     maskCopy ( world->mask[ e ], world->mask[ f ] );
                     maskCopy ( world->mask[ f ], COMPONENT_NONE );
 
+                    // Copy the component data of the old to the new entity.
                     unsigned int i = 0;
                     for ( i = 0; i < COMPONENT_COUNT; ++i )
                     {
@@ -91,21 +106,27 @@ void shrinkWorld ( World * world )
                         }
                     }
 
+                    // Start searching for the next blank.
                     break;
                 }
             }
         }
     }
+    // Set nextEntity to the next blank entity.
     world->nextEntity = f;
 }
 
+// Increase ENTITY_COUNT, increasing the maximum number of entities.
 void growWorld ( World * world )
 {
+    // Double ENTITY_COUNT.
     ENTITY_COUNT *= 2;
+    // Reallocate world for the new amount of memory.
     world->mask = realloc ( world->mask
                           , ENTITY_COUNT * sizeof ( *world->mask ) );
 
     int i = 0;
+    // Reallocate all of the arrays of components.
     for ( i = 0; i < COMPONENT_COUNT; ++i )
     {
         if ( world->component[ i ] != NULL )
--- world.h
+++ world.h
@@ -21,21 +21,28 @@
 
 #include "mask.h"
 
+// the maximum number of entities possible without growing
 extern unsigned int ENTITY_COUNT;
-#define COMPONENT_COUNT (32 * MASK_LENGTH)
+#define COMPONENT_COUNT (32 * MASK_LENGTH) // the maximum number of components
 
+// Holds state data for entities and components.
 struct World
 {
-    Mask * mask;
-    size_t componentSize[ COMPONENT_COUNT ];
+    Mask * mask; // masks for the component ownership of entities
+    size_t componentSize[ COMPONENT_COUNT ]; // array of sizes of components
+    // component data for all components; components will be typecast
     void * component[ COMPONENT_COUNT ];
-    unsigned int nextEntity;
+    unsigned int nextEntity; // the next entity to allocate
 };
 typedef struct World World;
 
+// Returns a new, empty world.
 World * newWorld ( void );
+// Frees the world.
 void freeWorld ( World * world );
+// Shift entities such that they fill contiguous space.
 void shrinkWorld ( World * world );
+// Increase ENTITY_COUNT, increasing the maximum number of entities.
 void growWorld ( World * world );
 
 #endif
